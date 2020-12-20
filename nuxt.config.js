67898f472eea388efe3cbd40247f30180e1cc606
Alireza <alirezamaleki944@gmail.com>
2020-12-20 12:57:39 +0330

    bootstrap and jqurey added -getbootstrap.com installation guids -navbar template created

---
+++ nuxt.config.js
@@ -0,0 +1,75 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'BoutiquePlus',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
+        integrity: 'sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1',
+        crossorigin: 'anonymous'
+      }
+    ],
+    script: [
+      { 
+        src: 'https://code.jquery.com/jquery-3.5.1.min.js'
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
+        integrity: 'sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW',
+        crossorigin: 'anonymous',
+        body: true
+      },
+      {
+        src: 'js/nav.js'
+      }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '~assets/css/bootstrap-rtl.min.css',
+    '~assets/css/navigation.css'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Content module configuration (https://go.nuxtjs.dev/config-content)
+  content: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <!--Navbar-->
+  <div class="container-fluid navbar-expand-lg bg-white sticky-top shadow-sm">
+    <div class="row py-1">
+      <!--login-->
+      <div class="col-5 col-sm-6 text-right login">
+        <button type="button" class="btn-login btn-sm btn">
+          <i class="material-icons d-inline-block">how_to_reg</i>
+          <span class="font-size-12 d-none d-sm-inline-block">????</span>
+        </button>
+
+        <a href="membership/addmems.html">
+          <i class="material-icons d-inline-block">person_add_alt_1</i>
+          <span class="font-size-12 d-none d-sm-inline-block">?????</span>
+        </a>
+      </div>
+      <!--/.login-->
+
+      <!--Cart-->
+      <div class="col-7 col-sm-6 text-left">
+        <ul class="cart">
+          <li>
+            <a
+              href=""
+              class="nav-link"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="???? ??????? ?? ???? ???"
+            >
+              <i class="far fa-heart ml-1"></i
+              ><span
+                class="font-size-11 text-color-silver d-none d-sm-inline-block"
+                >??????? ??(0)</span
+              >
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              class="nav-link"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="??? ???? ??? ???? ???"
+            >
+              <i class="fas fa-shopping-cart ml-1"></i
+              ><span
+                class="font-size-11 text-color-silver d-none d-sm-inline-block"
+                >??? ????(0)</span
+              >
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!--/.Cart-->
+    </div>
+
+    <div class="row">
+      <nav class="col-12 navbar navbar-expand-md navbar-light">
+        <!--Home Link Icon-->
+        <a href="#" class="navbar-brand d-none d-md-block" id="homeLink">
+          <i class="fas fa-home"></i>
+        </a>
+
+        <!--Collapse Button-->
+        <button
+          type="button"
+          class="navbar-toggler btn btn-sm order-0"
+          data-toggle="collapse"
+          data-target="#nav-set"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!--Navigation Menu-->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+                >Disabled</a
+              >
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+  <!--/.Navbar-->
+</template> 
+
+<script>
+export default {};
+</script>
+
+<style>
+body,html {
+  direction: rtl;
+}
+.rtl {
+  direction: rtl;
+}
+</style>
